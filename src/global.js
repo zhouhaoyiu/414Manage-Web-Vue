ba46d53f7ad876929cb4333a55673e8d95d43e82
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2021-06-30 21:39:42 +0800

    add deepcopy

--- src/global.js
+++ src/global.js
@@ -2,4 +2,22 @@ const pageList = [{
   'introduction': '/introduction'
 }]
 
+function deepClone(org, tar) {
+  var tar = tar || {}
+  var toStr = Object.prototype.toString
+  var arrType = '[object Array]'
+
+  for (let key in org) {
+    if (org.hasOwnProperty(key)) {
+      if (typeof (org[key]) === 'object' && org[key] !== null) {
+        tar[key] = toStr.call(org[key]) === arrType ? [] : {}
+        deepClone(org[key], tar[key])
+      } else {
+        tar[key] = org[key]
+      }
+    }
+  }
+  return tar
+}
+
 export default pageList
